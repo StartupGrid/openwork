c5077de75e52568f56036f1b0e46e4a3ffbf730d
Jacob Hayes <JacobHayes@Jacobs-MacBook-Pro.local>
2013-11-05 22:55:23 -0600

    Added Java for yum based systems. Resolves #1

--- shell/install_deps.sh
+++ shell/install_deps.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
-####
-# Install NLTK and download data
-####
-
 if [[ `whoami` != 'root' ]]
 then
    echo 'Script must run as root.'
    exit
 fi
 
+####
+# Installing package dependencies and Java
+####
+
 if (( `which yum >> /dev/null; echo $?` == 0  ))
 then
+   # Python dependencies
    yum -y install gcc python-devel
+
+   # Java JDK download and install
+   curl -L -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" -o jdk_7u45_x64.rpm "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.rpm" 
+   yum install jdk_7u45_x64.rpm
 elif (( `which apt-get >> /dev/null; echo $?` == 0 ))
 then
+   # Python dependencies 
    apt-get update
    apt-get -y install gcc python-setuptools python-dev python-software-properties
 
+   # Java Repo and install
    add-apt-repository -y ppa:webupd8team/java
    apt-get update
    echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
@@ -26,6 +33,10 @@ then
 fi
 
 echo -e '\n'
+
+####
+# Install Python regex and NLTK + download data
+####
 
 easy_install nltk regex
 
